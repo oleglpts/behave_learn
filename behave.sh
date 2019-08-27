748f5569c0f6be46f823d0a21a75a13eb3bb30dc
Oleg Lupats <oleglupats@gmail.com>
2019-08-27 12:38:23 +0300

    Fixed behave.sh

--- behave.sh
+++ behave.sh
@@ -3,6 +3,6 @@
 # https://docs.qameta.io/allure/#_jenkins - Jenkins integration
 
 source venv/bin/activate
-rm -rf reports/*
+# rm -rf reports/*
 behave -f allure_behave.formatter:AllureFormatter -o reports/$BUILD_NUMBER
 deactivate
