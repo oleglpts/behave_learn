41d0757a827d44402246628431851d151cd9c382
Oleg Lupats <oleglupats@gmail.com>
2021-12-23 22:48:17 +0200

    Debug

--- behave.sh
+++ behave.sh
@@ -5,6 +5,7 @@
 source venv/bin/activate
 rm -rf reports/*
 mkdir reports
+chown jenkins:jenkins reports
 #behave -f allure_behave.formatter:AllureFormatter -o reports/$BUILD_NUMBER
 behave -f pretty -o reports/$BUILD_NUMBER
 deactivate
