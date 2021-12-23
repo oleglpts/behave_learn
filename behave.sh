3145898c09cf1099f2dfe02ada8bc99db76d17dc
Oleg Lupats <oleglupats@gmail.com>
2021-12-23 22:11:06 +0200

    Allure behave problem

--- behave.sh
+++ behave.sh
@@ -3,6 +3,7 @@
 # https://docs.qameta.io/allure/#_jenkins - Jenkins integration
 
 source venv/bin/activate
-# rm -rf reports/*
-behave -f allure_behave.formatter:AllureFormatter -o reports/$BUILD_NUMBER
+rm -rf reports/*
+#behave -f allure_behave.formatter:AllureFormatter -o reports/$BUILD_NUMBER
+behave -f pretty -o reports/$BUILD_NUMBER
 deactivate
